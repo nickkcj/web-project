5a205d9d7ef7521250e7bf0c784a21c64ecd4533
nicolasmichielon <nicolasmichielon@gmail.com>
2025-05-27 20:54:51 -0300

    refactor: correct import path in services and fix login url

--- frontend/src/services/services.tsx
+++ frontend/src/services/services.tsx
@@ -1,37 +1,37 @@
 import axios from "axios";
-import { PATH } from "../PATH";
+import { PATH } from "../path";
 
 const services = {
   registerUser: async (body: { name: string; email: string; password: string }) => {
     try {
-        const response = await axios.post(`${PATH.base}/users`, {
-          name: body.name,
-          email: body.email,
-          password: body.password,
-        });
-    
-        return response.data;
-      } catch (err: any) {
-        if (err.response) {
-          if (err.response.data && Array.isArray(err.response.data)) {
-            const errorMessage = err.response.data[0];
-            if (errorMessage === "This email is already in use.") {
-              throw new Error("Este e-mail já está em uso.");
-            } else {
-              throw new Error(errorMessage);
-            }
+      const response = await axios.post(`${PATH.base}/users`, {
+        name: body.name,
+        email: body.email,
+        password: body.password,
+      });
+
+      return response.data;
+    } catch (err: any) {
+      if (err.response) {
+        if (err.response.data && Array.isArray(err.response.data)) {
+          const errorMessage = err.response.data[0];
+          if (errorMessage === "This email is already in use.") {
+            throw new Error("Este e-mail já está em uso.");
           } else {
-            throw new Error("Erro inesperado. Tente novamente.");
+            throw new Error(errorMessage);
           }
         } else {
-          throw new Error("Um erro ocorreu. Tente novamente mais tarde.");
+          throw new Error("Erro inesperado. Tente novamente.");
         }
+      } else {
+        throw new Error("Um erro ocorreu. Tente novamente mais tarde.");
+      }
     }
   },
 
   loginUser: async (body: { email: string; password: string }) => {
     try {
-      const response = await axios.post(`${PATH.base}/login`, {
+      const response = await axios.post(`${PATH.base}/users/login`, {
         email: body.email,
         password: body.password,
       });
