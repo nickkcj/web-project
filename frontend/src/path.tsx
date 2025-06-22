39ca14ffda3c92e927334297802bc822e171714c
Felipe041198 <felipe041198@hotmail.com>
2025-06-22 17:04:46 -0300

    Update path.tsx

--- frontend/src/path.tsx
+++ frontend/src/path.tsx
@@ -1,7 +1,13 @@
 //faz a conexção com o backend, colocando o link de onde ele ta hospedado
 const baseurl = {
-  apiUrl: "https://web-project-k6x9.onrender.com/api",
+  apiUrl: "http://localhost:3001/api",
 };
 export const PATH = {
   base: baseurl.apiUrl,
+  MOVIES: {
+    ROOT: "/movies",
+    POPULAR: "/movies/popular",
+    SEARCH: "/movies/search",
+  },
+  REVIEWS: "/reviews",
 };
